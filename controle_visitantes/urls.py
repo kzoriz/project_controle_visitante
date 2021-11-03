5a1879792d5be518358b36acd86476f4a4646660
kzoriz <kzoriz@outlook.com>
2021-11-03 17:41:42 -0300

    iniciando cap8

--- controle_visitantes/urls.py
+++ controle_visitantes/urls.py
@@ -16,8 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from usuarios.views import  index
-
+import visitantes.views
 urlpatterns = [
     path('admin/', admin.site.urls),   
     path("",index, name="index"),
+    path("registrar-visitante/",visitantes.views.registrar_visitante,name="registrar_visitante"),
 ]

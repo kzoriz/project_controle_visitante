2faf836e1a50620882338a9a5afbb2f0103fb768
kzoriz <kzoriz@outlook.com>
2021-11-03 20:04:45 -0300

    Conhecendo um pouco mais dos formulários

--- visitantes/models.py
+++ visitantes/models.py
@@ -5,7 +5,7 @@ from django.db.models.fields.related import ForeignKey
 class Visitante(models.Model):
     nome_completo= models.CharField(verbose_name="Nome completo", max_length=194)
     cpf=models.CharField(verbose_name="CPF",max_length=11)
-    data_nascimento=models.DateField(verbose_name="Dta de nascimento", auto_now=False, auto_now_add=False)
+    data_nascimento=models.DateField(verbose_name="Data de nascimento", auto_now=False, auto_now_add=False)
     numero_casa=models.PositiveIntegerField(verbose_name="Numero da casa a ser visitada")
     placa_veiculo= models.CharField(verbose_name="Placa do veiculo", max_length=7,null= True, blank=True)
     horario_chegada=models.DateTimeField(verbose_name="Horario de chegada na portaria",auto_now_add=True)
